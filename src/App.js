44ab6c933ba55937519f3758bf600e09b8cc83ce
Cindy Ravell <cindy.ravell@theksquaregroup.com>
2021-03-29 13:09:39 -0600

    Finish the first part

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="container">
+      <header className="header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className = "title">Cindy Ravellously</h1>
+        
       </header>
+      <body className= "body">
+        <div>
+          <img id = "imagenCindy" class="Perfil" src="/Ravell.jpg" alt="Cindy" height="500px"/>
+          
+          <div id="resume">
+          <h2 id="resume">Mechatronic engineer</h2>
+          <p>Projects in which I participated: Siglo XXI, Hospital Eme Red, Crío, Fraccionamiento Punta Esmeralda, Centro de Salud con Servicios amppos Progreso</p>
+
+          <div class = "txt">
+            <p>Social Responsibipty</p>
+            <p>Respectful and professional person</p>
+            <p>Perseverance to understand new topics and develop new skills </p>
+            <p>Computer programming</p>
+            <p>Automation Systems</p>
+            <p>Mechanics</p>
+            <p>Empathy</p>
+            <p>Teamwork</p>
+          </div>
+
+
+          
+          </div>
+          
+
+        </div>
+      </body>
     </div>
   );
 }
